0d73c6026cc20f04f080256098a3365ad79ed14e
mfbevan <matthewbevan00@gmail.com>
2024-07-19 14:55:52 +1000

    feat: ✨ cross node communication (ping pong)

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import "github.com/mfbevan/go-p2p/node"
 
 func main() {
-	config := node.Config{Port: 61877}
+	config := node.Config{}
 	n := node.New(config)
 	n.Start()
+	n.PrintInfo()
+	n.SetupPeers()
 }
--- node/node.go
+++ node/node.go
@@ -1,6 +1,7 @@
 package node
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"os/signal"
@@ -8,7 +9,9 @@ import (
 
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/core/host"
+	peerstore "github.com/libp2p/go-libp2p/core/peer"
 	"github.com/libp2p/go-libp2p/p2p/protocol/ping"
+	multiaddr "github.com/multiformats/go-multiaddr"
 )
 
 type Config struct {
@@ -16,8 +19,9 @@ type Config struct {
 }
 
 type Node struct {
-	Port int
-	Node host.Host
+	Port        int
+	Node        host.Host
+	PingService *ping.PingService
 }
 
 // Create a new node with the given configuration
@@ -28,6 +32,7 @@ func New(config Config) *Node {
 
 // Start the node
 func (n *Node) Start() {
+	fmt.Println("Starting node...")
 	address := fmt.Sprintf("/ip4/127.0.0.1/tcp/%d", n.Port)
 	node, err := libp2p.New(
 		libp2p.ListenAddrStrings(address),
@@ -39,22 +44,15 @@ func (n *Node) Start() {
 	}
 
 	n.Node = node
-
-	fmt.Println("Listen addresses:", node.Addrs())
-
-	n.setupTerminator()
+	n.SetupPingService()
 }
 
-// Setup the signal handler to stop the node
-func (n *Node) setupTerminator() {
+// Setup the signal handler to stop the node when a SIGINT or SIGTERM is received
+func (n *Node) AwaitTerminate() {
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
 	<-ch
 	fmt.Println("Received signal, shutting down...")
-
-	if err := n.Node.Close(); err != nil {
-		panic(err)
-	}
 }
 
 // Stop the node
@@ -63,7 +61,53 @@ func (n *Node) Stop() {
 }
 
 // Setup the ping service
-func (n *Node) SetupPingService() {
+func (n *Node) SetupPingService() *ping.PingService {
 	pingService := &ping.PingService{Host: n.Node}
 	n.Node.SetStreamHandler(ping.ID, pingService.PingHandler)
+
+	n.PingService = pingService
+	return pingService
+}
+
+// Print the information for this peer node in multiaddr format
+func (n *Node) PrintInfo() {
+	peerInfo := peerstore.AddrInfo{
+		ID:    n.Node.ID(),
+		Addrs: n.Node.Addrs(),
+	}
+	addrs, err := peerstore.AddrInfoToP2pAddrs(&peerInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Node Address:", addrs[0])
+}
+
+func (n *Node) SetupPeers() {
+	if len(os.Args) > 1 {
+		fmt.Println("Connecting to peer", os.Args[1])
+		// Get the multiaddress from the command line arguments
+		addr, err := multiaddr.NewMultiaddr(os.Args[1])
+		if err != nil {
+			panic(err)
+		}
+		// Create a new peer from the multiaddress
+		peer, err := peerstore.AddrInfoFromP2pAddr(addr)
+		if err != nil {
+			panic(err)
+		}
+		// Connect to the peer
+		if err := n.Node.Connect(context.Background(), *peer); err != nil {
+			panic(err)
+		}
+		// Send 5 ping messages to the peer
+		fmt.Println("Sending 5 Ping Messages to ", addr)
+		ch := n.PingService.Ping(context.Background(), peer.ID)
+		for i := 0; i < 5; i++ {
+			res := <-ch
+			fmt.Println("pinged", addr, "in", res.RTT)
+		}
+	} else {
+		n.AwaitTerminate()
+	}
 }
